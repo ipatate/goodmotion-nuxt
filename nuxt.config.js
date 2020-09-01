80d56610c975410d2a87af4766b33de5b998abb5
Patrick Faramaz <faramazpat@gmail.com>
2020-09-01 13:36:26 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default {
+  modules: ["@nuxt/content"],
+  content: {
+    // Options
+  },
+  storybook: {
+    // Options
+  },
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
